90172842786795f44f3d0ba752d7adc36c98d310
Hamid <hamid255990@gmail.com>
2025-06-19 02:49:30 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+birth_year= input("Birth_year: ")
+#print(type(birth_year))
+age= 2025 -int(birth_year)
+print(type(age))
+print(age)
+# Ask  a user their weight (in kg), convert it to pound and print 
+#on the terminal.
+weight_kg = input("Enter your weight in kilograms: ")
+weight_pound = float(weight_kg) * 2.20462
+print("Your weight in pounds is:", weight_pound)
